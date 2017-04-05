766cd03ebe0775030ff93c4ba369cdf2522ec027
Maxim Zelenkin <nudepatch@gmail.com>
2017-04-05 23:00:08 +0300

    First primitive concept

--- src/App.js
+++ src/App.js
@@ -3,16 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      messages: [
+        {id: 1, body: 'Hi', nested_messages: []}
+      ]
+    }
+  }
+
+  loadMessages(evt) {
+    this.setState({
+      messages: [
+        {id: 1, body: 'Hi', nested_messages: [
+          {id: 2, body: 'Hi2'},
+          {id: 3, body: 'Hi3'}
+        ]}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Dialogs constructor</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.messages.map(message =>
+          <div key={message.id} className="Message-Box">
+            <div className="Message-Row">
+              <div className="Message" onClick={this.loadMessages.bind(this)}>
+                {message.body}
+              </div>
+            </div>
+            <div className="Message-Row">
+            {message.nested_messages.map(message =>
+              <div key={message.id} className="Message">
+                {message.body}
+              </div>
+            )}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
